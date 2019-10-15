b9e7b7afe8c490f6414ddd434f9d255b2c8362e1
mastercoks <matheusbdo1@gmail.com>
2019-10-15 01:31:30 -0300

    Funcionalidades e interface prontas

--- bingo.js
+++ bingo.js
@@ -1,69 +1,156 @@
-import elements from "./playerElements.js";
+import elements from "./elements.js";
 import player from "./player.js";
 import { getNumberNotRepeated } from "./utils.js";
-import { cardTemplate, numberDrawnTemplate } from "./components/templates.js";
+import { cardTemplate, numbersDrawnTemplate, rankingItemTemplate, rankingTemplate, infoSVG, trophySVG } from "./components/templates.js";
 
-const state = {
-  CRIANDO: 0,
-  SORTEANDO: 1,
-  PREMIADO: 2
+const states = {
+  REGISTER: 0,
+  DRAWN: 1,
+  WINNER: 2
+}
+
+const types = {
+  INFO: 0,
+  TROPHY: 1
 }
 
 export default {
+  expand: true,
+  showModal: false,
   players: [],
   numbersDrawn: [],
   valueMin: 1,
   valueMax: 25,
   qtyNumbers: 8,
-  state: state.CRIANDO,
+  state: states.REGISTER,
+
   start() {
     elements.get.call(this);
     elements.actions.call(this);
-    this.drawNumber.disabled = true;
+    this.update();
   },
-  addCard() {
-    if (this.drawNumber.disabled) this.drawNumber.disabled = false;
-    if (this.name.value) {
-      const playerData = new player(this.players.length+1, this.name.value, this.valueMin, this.valueMax, this.qtyNumbers);
-      console.log(playerData);
-      
-      this.players.push(playerData);
-      this.cardsEl.innerHTML += cardTemplate(playerData.name, playerData.numbers, this.numbersDrawn);
-    } else {
-      alert('Preencha o campo!')
-    }
+
+  restart() {
+    this.players = [];
+    this.numbersDrawn = [];
+    this.state = states.REGISTER;
+    this.update();
   },
-  draw() {
-    if (this.players.length === 0) return alert('Não existe nenhum jogador');
-    
-    if (this.state === state.CRIANDO) {
-      this.state = state.SORTEANDO;
-      this.name.disabled = true;
-      this.generateCard.disabled = true;
-    }
-    
-    let number = getNumberNotRepeated(this.valueMin, this.valueMax, this.numbersDrawn);
-    if (number) {
-      this.numbersDrawn.push(number);
-      this.numbersDrawnEl.innerHTML += numberDrawnTemplate(number);
-      console.log(this.numbersDrawn);
-    } else {
-      console.log('Todos números já foram sorteados');      
-    }        
-    this.update(number);
-  },
-  update(numberDrawn) {
+
+  update() {    
+
+    const size = this.numbersDrawn.length;
+
     this.cardsEl.innerHTML = '';
-    console.log(this.players);
+    
     
     for (let playerData of this.players) {
-      this.checkHit(playerData, numberDrawn);
-      this.cardsEl.innerHTML += cardTemplate(playerData.name, playerData.numbers, this.numbersDrawn);
+      this.checkHit(playerData, this.numbersDrawn[size-1]);
+      this.cardsEl.innerHTML += cardTemplate(playerData, this.numbersDrawn);
+    }
+
+    this.lastEl.innerText = this.numbersDrawn[size-1] ? this.numbersDrawn[size-1] : '';
+    this.lastButOneEl.innerText = this.numbersDrawn[size-2] ? this.numbersDrawn[size-2] : '';
+    this.lastButTwoEl.innerText = this.numbersDrawn[size-3] ? this.numbersDrawn[size-3] : '';
+    this.lastButThreeEl.innerText = this.numbersDrawn[size-4] ? this.numbersDrawn[size-4] : '';
+    this.lastButFourEl.innerText = this.numbersDrawn[size-5] ? this.numbersDrawn[size-5] : '';
+    this.lastButFiveEl.innerText = this.numbersDrawn[size-6] ? this.numbersDrawn[size-6] : '';
+
+    this.numbersDrawnEl.innerHTML = numbersDrawnTemplate(this.numbersDrawn, this.valueMax);
+    
+    this.rankingBodyEl.innerHTML = rankingTemplate(this.players);
+
+    if (this.state === states.REGISTER) {
+      this.inputName.disabled = false;
+      this.buttonGenerate.disabled = false;
+      this.buttonDrawn.disabled = true;
+      this.buttonRestart.disabled = true;
+    } else if (this.state === states.DRAWN) {
+      this.inputName.disabled = true;
+      this.buttonGenerate.disabled = true;
+      this.buttonDrawn.disabled = false;
+      this.buttonRestart.disabled = true;
+    } else {
+      this.toggleModal(types.TROPHY);
+      this.inputName.disabled = true;
+      this.buttonGenerate.disabled = true;
+      this.buttonDrawn.disabled = true;
+      this.buttonRestart.disabled = false;
+    }    
+  },
+
+  addCard() {
+    if (this.inputName.value) {
+      if (this.buttonDrawn.disabled) this.buttonDrawn.disabled = false;
+
+      const playerData = new player(this.players.length+1, this.inputName.value, this.valueMin, this.valueMax, this.qtyNumbers);
+      
+      this.players.push(playerData);
+      this.cardsEl.innerHTML += cardTemplate(playerData, this.numbersDrawn);
+      this.rankingBodyEl.innerHTML += rankingItemTemplate(playerData);
+      this.inputName.value = '';
+    } else {
+      this.toggleModal(types.INFO, 'Preencha o campo nome!');
     }
   },
+
+  draw() {
+    if (this.players.length === 0) return this.toggleModal(types.INFO, 'É necessário ter algum jogador para iniciar o sorteio!');
+    
+    if (this.state === states.REGISTER) this.state = states.DRAWN;
+    
+    let number = getNumberNotRepeated(this.valueMin, this.valueMax, this.numbersDrawn);
+    if (number) this.numbersDrawn.push(number);    
+
+    this.update();
+  },
+
   checkHit(playerData, numberDrawn) {
     for (const number of playerData.numbers) {
       if (numberDrawn === number) playerData.score++;
+    }
+    if (playerData.score === this.qtyNumbers) this.state = states.WINNER;
+  },
+
+  getWinners() {
+    let winners = [];
+    for (const playerData of this.players) {
+      if (playerData.score === this.qtyNumbers) {
+        winners.push(playerData.name);
+      }
+    }
+    return winners;
+  },
+
+  toggleExpandRanking() {
+    this.expand = !this.expand;
+    if (this.expand) {
+      this.expandRanking.innerText = "expand_less";
+      this.ranking.classList.remove("hide");
+    } else {
+      this.expandRanking.innerText = "expand_more";
+      this.ranking.classList.add("hide");
+    }
+  },
+
+  toggleModal(type, msg) {
+
+    this.modalIcon.innerHTML = '';
+    if (type === types.INFO) {
+      this.modalIcon.innerHTML = infoSVG();
+      this.modalTitle.innerText = 'Atenção';
+      this.modalText.innerText = msg;
+    } else {
+      const winners = this.getWinners();
+      this.modalIcon.innerHTML = trophySVG();
+      this.modalTitle.innerText = winners.length > 1 ? 'Parabéns aos vencedores' : 'Parabéns ao vencedor';
+      this.modalText.innerText = winners.toString().split(',').join('\n');
+    }
+    this.showModal = !this.showModal;
+    if (this.showModal) {
+      this.modalInfo.classList.remove("hide");
+    } else {
+      this.modalInfo.classList.add("hide");
     }
   }
 }
--- utils.js
+++ utils.js
@@ -1,3 +1,25 @@
+const colors = [
+  'red',
+  'pink',
+  'purple',
+  'deep-purple',
+  'indigo',
+  'blue',
+  'light-blue',
+  'cyan',
+  'teal',
+  'green',
+  'light-green',
+  'lime',
+  'yellow',
+  'amber',
+  'orange',
+  'deep-orange',
+  'brown',
+  'gray',
+  'blue-gray'
+]; 
+
 const getRandomNumber = (valueMin, valueMax) => 
   Math.floor(Math.random() * (valueMax - valueMin + 1) + valueMin);
 
@@ -13,5 +35,7 @@ const getNumberNotRepeated = (valueMin, valueMax, list) => {
   return number;
 }
 
-export { getNumberNotRepeated };
+const getRandomColor = () =>  colors[getRandomNumber(0,colors.length-1)]
+
+export { getNumberNotRepeated, getRandomColor };
 
